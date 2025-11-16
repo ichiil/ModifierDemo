baf25431e539f385f700a7dadff017daed7f4b51
ichiil <bloodgamer776@gmail.com>
2025-11-17 04:57:06 +0700

    Добавление модификаторов в DemoScreen

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,6 +16,7 @@ import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +34,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
+
+    val mymodifier = modifier
+        .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
+
     Text(
         "Hello Compose",
+        mymodifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
